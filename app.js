20ff532201dccae4ccd8b47ec5d276b2ab2bf494
Karin Alvarez <karin@Josiah-Haswells-MacBook.local>
2014-03-16 19:06:28 -0600

    Able to post

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/addDogDirection', function (req, res) {
 	res.redirect('/addDog')
 })
 
-app.get("/addDogAction", contentController.addDog)
+app.post("/addDogAction", contentController.addDog)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
